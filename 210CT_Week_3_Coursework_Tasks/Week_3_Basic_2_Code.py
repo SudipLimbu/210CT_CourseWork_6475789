fa88faef46cb9235a96e3f58112131125848e313
SudipLimbu <limbus5@uni.coventry.ac.uk>
2016-12-02 22:57:46 +0000

    Add files via upload

--- 210CT_Week_3_Coursework_Tasks/Week_3_Basic_2_Code.py
+++ 210CT_Week_3_Coursework_Tasks/Week_3_Basic_2_Code.py
@@ -1,8 +1,40 @@
-i = int(input("Input: "))
+n=int(input("please: "))
+def PrimeNumber (p, n): 
+    if n==2:
+        print("2 is prime")
+        
 
-while i>0:
-    if i%2==0:
-        a =print("not prime")
-        i=i-1
+    elif n < 2 or (n % 2) == 0:
+        print("Isn't a prime")
+        
+
+    elif p >= n or n == p:
+        print("Is a prime")
+
+    elif n %11==0:
+        print("not prime")
+
+    else:
+        return PrimeNumber(p+1,n)
+
     
+PrimeNumber(2,n)
+
+
+"""
+PRIMENUMBER(p,n)
+    IF n =2 THEN
+      OUTPUT (STRING)
+    ELIF n<2 OR n%2 = 0 THEN
+      OUTPUT(STRING)
+    ELIF p >= n OR n == p THEN
+        OUTPUT(STRING)
+
+    ELIF n %11=0 THEN
+        OUTPUT(STRING)
+
+    ELSE THEN
+        RETURN PRIMENUMBER(p+1,n)
+
     
+PRIMENUMBER(2,n)
--- 210CT_Week_3_Coursework_Tasks/Week_3_Basic_3_Code.py
+++ 210CT_Week_3_Coursework_Tasks/Week_3_Basic_3_Code.py
@@ -1,6 +1,20 @@
 def remove(x):
-    vowel= ("a","e","i","o","u")
+    empty=[]
+    vowel=["a","e","i","o","u"]
+
     for i in x:
-        if i in vowel:
-            c = x.replace(i,"")
-    return c
+        if i not in vowel:
+            empty.append(i)
+    print(''.join(empty))
+
+remove("beautiful")
+
+"""REMOVE(X)
+    empty<-[LIST]
+    vowel<-["a","e","i","o","u"]
+
+    FOR i IN X
+        if i NOT IN vowel
+            emty.ammend(i)
+    OUTPUT(''.join(empty))
+    """
